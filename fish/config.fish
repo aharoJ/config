fcb64dd1bb6f15b405f374dc406836d519d02030
Angel J. Haro <haro.j.angel@gmail.com>
2024-03-09 18:13:37 -0800

    ...aharoJ

--- fish/config.fish
+++ fish/config.fish
@@ -1,42 +1,47 @@
-
+######## Commands to run in interactive sessions can go here ########
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -x PATH /usr/local/bin $PATH
+    
+    # Set the EDITOR
+    set -x EDITOR lvim
 
-    # Rust stuff
-    set -x PATH "$HOME/.cargo/bin" $PATH
-
-
-    starship init fish | source
+    # Set the fish greeting
     set fish_greeting
 
+    # Initialize Starship prompt
+    starship init fish | source
+
+    # Set Rust's cargo bin path
+    set -x PATH "$HOME/.cargo/bin" $PATH
+
+    # Set Python path
     set -gx PATH /Library/Frameworks/Python.framework/Versions/3.8/bin $PATH
+    
+    # Set PYENV_ROOT and add to user paths
     set -Ux PYENV_ROOT $HOME/.pyenv
     set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-    pyenv init - | source
+    
+    # Initialize pyenv
+    if type pyenv > /dev/null 2>&1
+        pyenv init - | source
+    end
 
-    alias vim lvim
-    set -x EDITOR lvim
+    # Set Emacs path
     set -gx PATH "/Applications/MacPorts/Emacs.app/Contents/MacOS" $PATH
+
+    # Set the Starship configuration path
     set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
 
+    # Set local bin path created by `pipx`
+    set -x PATH $PATH /Users/aharo/.local/bin
 
-
+    # Set JAVA_HOME and add Java binary to PATH
+    set -gx JAVA_HOME (brew --prefix openjdk@17)
+    set -gx PATH $JAVA_HOME/bin $PATH
 end
+#################################################################################
 
-# Created by `pipx` on 2023-08-18 22:06:32
-set PATH $PATH /Users/aharo/.local/bin
-
-
-# # JAVA_HOME configuration
-# set -gx JAVA_HOME /usr/local/opt/openjdk@11/libexec/openjdk.jdk/Contents/Home
-
-# # Add Java binary to PATH
-# set -gx PATH $JAVA_HOME/bin $PATH
-
-
-
-
-set -gx JAVA_HOME (brew --prefix openjdk@17)
-set -gx PATH $JAVA_HOME/bin $PATH
+# Set lvim as default editor for Vim commands
+function vim
+    lvim $argv
+end
 
