1f024beafda64b5dc75e255dff6164ec26354385
Angel J. Haro <haro.j.angel@gmail.com>
2024-09-11 23:08:09 -0700

    hlchunk: remove error visual bar

--- nvim/lua/plugins/hlchunk.lua
+++ nvim/lua/plugins/hlchunk.lua
@@ -1,38 +1,38 @@
 return {
-  "shellRaining/hlchunk.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("hlchunk").setup({
-      chunk = {
-        enable = true, -- Ensure this is a table, not just a boolean
-        priority = 15,
-        style = {
-          { fg = "#806d9c" },
-          { fg = "#c21f30" },
-        },
-        use_treesitter = true,
-        chars = {
-          horizontal_line = "─",
-          vertical_line = "│",
-          left_top = "╭",
-          left_bottom = "╰",
-          right_arrow = ">",
-        },
-        textobject = "",
-        max_file_size = 1024 * 1024,
-        error_sign = true,
-        duration = 200,
-        delay = 300,
-      },
-      indent = {
-        enable = false, -- You can set this to true if you want to enable the indent feature
-      },
-      line_num = {
-        enable = false, -- Disable or configure if needed
-      },
-      blank = {
-        enable = false, -- Disable or configure if needed
-      },
-    })
-  end,
+	"shellRaining/hlchunk.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("hlchunk").setup({
+			chunk = {
+				enable = true, -- Ensure this is a table, not just a boolean
+				priority = 15,
+				style = {
+					{ fg = "#806d9c" },
+					{ fg = "#c21f30" },
+				},
+				use_treesitter = true,
+				chars = {
+					horizontal_line = "─",
+					vertical_line = "│",
+					left_top = "╭",
+					left_bottom = "╰",
+					right_arrow = ">",
+				},
+				textobject = "",
+				max_file_size = 1024 * 1024,
+				error_sign = false, -- cool but annoying to the eye for concentration
+				duration = 200,
+				delay = 300,
+			},
+			indent = {
+				enable = false, -- You can set this to true if you want to enable the indent feature
+			},
+			line_num = {
+				enable = false, -- Disable or configure if needed
+			},
+			blank = {
+				enable = false, -- Disable or configure if needed
+			},
+		})
+	end,
 }
