265f18b2a2f8514a5156004fb5f3c48a4b627937
Angel J. Haro <haro.j.angel@gmail.com>
2025-01-25 14:18:56 -0800

    python lsp testing

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,28 @@
 vim.g.mapleader = " " -- Spacebar as leader key
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-vim.g.loaded_netrw = 1       -- need to load before neo-tree
+vim.g.loaded_netrw = 1 -- need to load before neo-tree
 vim.g.loaded_netrwPlugin = 1 -- need to load before neo-tree
 require("lazy").setup("plugins", {
-  change_detection = {
-    notify = false,
-  },
+	change_detection = {
+		notify = false,
+	},
 })
-
 
 require("settings")
 vim.o.cmdheight = 1
 
+-- UNSURE IF I NEED THIS LINE (TESTING)
+-- vim.g.python3_host_prog = "/usr/local/opt/python@3.13/bin/python3"
 
-
-
-
--- HELLO WORLD! (: 
+-- HELLO WORLD! (:
