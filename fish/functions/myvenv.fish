0180467fcd900649c42cf117e9e1da3ef4e99d6d
Angel J. Haro <haro.j.angel@gmail.com>
2025-01-24 17:06:53 -0800

    fish

---
+++ fish/functions/myvenv.fish
@@ -0,0 +1,4 @@
+function myvenv --wraps='source /home/shyney/.venv/venvname/bin/activate.fish' --description 'alias myvenv=source /home/shyney/.venv/venvname/bin/activate.fish'
+  source /home/shyney/.venv/venvname/bin/activate.fish $argv
+        
+end
