7ed147228ddb370468d233d47b1bae39de9c1a14
Angel J. Haro <haro.j.angel@gmail.com>
2025-01-31 14:28:13 -0800

    clean up

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,20 +1,3 @@
--- ---@diagnostic disable: undefined-doc-name
--- return {
--- 	"folke/noice.nvim",
--- 	dependencies = {
--- 		'stevearc/dressing.nvim',
--- 		"MunifTanjim/nui.nvim",
--- 		'rcarriga/nvim-notify'
--- 	},
--- 	enabled = true,
--- 	config = function ()
--- 		require("plugins.ui-configs.dressing")
--- 		require("plugins.ui-configs.noice")
--- 		require("plugins.ui-configs.notify")
--- 	end
--- }
-
--- lazy.nvim
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
@@ -22,7 +5,7 @@ return {
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"rcarriga/nvim-notify",
-		"stevearc/dressing.nvim",
+		-- "stevearc/dressing.nvim",
 	},
 	config = function()
 		-- require("plugins.ui-configs.dressing")
