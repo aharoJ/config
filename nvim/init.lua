756a162a5325e207136443a4049b59099d62890c
Angel J. Haro <haro.j.angel@gmail.com>
2025-01-31 14:28:31 -0800

    optimization

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,9 @@
+-- Load settings from lua/settings.lua
+require("settings")
+
 vim.g.mapleader = " " -- Spacebar as leader key
+
+-- Bootstrap Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -11,15 +16,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Disable netrw (for neo-tree)
 vim.g.loaded_netrw = 1 -- need to load before neo-tree
 vim.g.loaded_netrwPlugin = 1 -- need to load before neo-tree
+
+-- Load plugins from lua/plugins.lua
 require("lazy").setup("plugins", {
 	change_detection = {
 		notify = false,
 	},
 })
 
-require("settings")
+-- Misc config
 vim.o.cmdheight = 1
-
--- HELLO WORLD! (:
