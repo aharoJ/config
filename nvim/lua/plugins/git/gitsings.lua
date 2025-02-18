8d2be6f5cbede88b09087eb160311835489d6dc0
Angel J. Haro <haro.j.angel@gmail.com>
2025-02-17 20:05:47 -0800

    git/ | git

--- nvim/lua/plugins/git/gitsings.lua
+++ nvim/lua/plugins/git/gitsings.lua
@@ -1,100 +1,105 @@
-require("gitsigns").setup({
-	signs = {
-		add = { text = "│" },
-		change = { text = "│" },
-		delete = { text = "_" },
-		topdelete = { text = "‾" },
-		changedelete = { text = "~" },
-		untracked = { text = "┆" },
-	},
-	signcolumn = true, -- nice bar on the left
-	numhl = false, -- turns numbers into higlight groups
-	linehl = false, -- highlights sections
-	word_diff = false,
-	watch_gitdir = {
-		follow_files = true,
-	},
-	auto_attach = true,
-	attach_to_untracked = false,
-	current_line_blame = false, -- the little message saying who committed the line
-	current_line_blame_opts = {
-		virt_text = true,
-		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-		delay = 1000,
-		ignore_whitespace = false,
-		virt_text_priority = 100,
-	},
-	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-	sign_priority = 6,
-	update_debounce = 100,
-	status_formatter = nil, -- Use default
-	max_file_length = 40000, -- Disable if file is longer than this (in lines)
-	preview_config = {
-		-- Options passed to nvim_open_win
-		border = "single",
-		style = "minimal",
-		relative = "cursor",
-		row = 0,
-		col = 1,
-	},
-	yadm = {
-		enable = false,
-	},
-})
-local gs = package.loaded.gitsigns
+return {
+	"lewis6991/gitsigns.nvim",
+	event = "BufReadPre",
+	config = function()
+		require("gitsigns").setup({
+			signs = {
+				add = { text = "│" },
+				change = { text = "│" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signcolumn = true, -- nice bar on the left
+			numhl = false, -- turns numbers into higlight groups
+			linehl = false, -- highlights sections
+			word_diff = false,
+			watch_gitdir = {
+				follow_files = true,
+			},
+			auto_attach = true,
+			attach_to_untracked = false,
+			current_line_blame = false, -- the little message saying who committed the line
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+		})
+		local gs = package.loaded.gitsigns
 
-local function map(mode, l, r, opts)
-	opts = opts or {}
-	opts.buffer = bufnr
-	vim.keymap.set(mode, l, r, opts)
-end
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
 
--- Navigation
-map("n", "]c", function()
-	if vim.wo.diff then
-		return "]c"
+		-- Navigation
+		map("n", "]c", function()
+			if vim.wo.diff then
+				return "]c"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		map("n", "[c", function()
+			if vim.wo.diff then
+				return "[c"
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
+
+		-------------------        CUSTOM TOGGLE       ------------------------
+		local enabled = true -- default state, adjust as needed
+		function ToggleGitsigns()
+			local gitsigns = require("gitsigns")
+			enabled = not enabled
+
+			if enabled then
+				gitsigns.toggle_signs(true)
+				gitsigns.toggle_numhl(true)
+				gitsigns.toggle_linehl(true)
+				gitsigns.toggle_word_diff(true)
+				gitsigns.toggle_current_line_blame(true)
+			else
+				-- Disable all or specific gitsigns features
+				gitsigns.toggle_signs(false)
+				gitsigns.toggle_numhl(false)
+				gitsigns.toggle_linehl(false)
+				gitsigns.toggle_word_diff(false)
+				gitsigns.toggle_current_line_blame(false)
+			end
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>tg", ":lua ToggleGitsigns()<CR>", { noremap = true, silent = true })
+		-- UNSURE
+		vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#D2E59D" })
 	end
-	vim.schedule(function()
-		gs.next_hunk()
-	end)
-	return "<Ignore>"
-end, { expr = true })
+}
 
-map("n", "[c", function()
-	if vim.wo.diff then
-		return "[c"
-	end
-	vim.schedule(function()
-		gs.prev_hunk()
-	end)
-	return "<Ignore>"
-end, { expr = true })
 
--------------------        CUSTOM TOGGLE       ------------------------
-local enabled = true -- default state, adjust as needed
-function ToggleGitsigns()
-	local gitsigns = require("gitsigns")
-	enabled = not enabled
-
-	if enabled then
-		gitsigns.toggle_signs(true)
-		gitsigns.toggle_numhl(true)
-		gitsigns.toggle_linehl(true)
-		gitsigns.toggle_word_diff(true)
-		gitsigns.toggle_current_line_blame(true)
-	else
-		-- Disable all or specific gitsigns features
-		gitsigns.toggle_signs(false)
-		gitsigns.toggle_numhl(false)
-		gitsigns.toggle_linehl(false)
-		gitsigns.toggle_word_diff(false)
-		gitsigns.toggle_current_line_blame(false)
-	end
-end
-
-vim.api.nvim_set_keymap("n", "<leader>tg", ":lua ToggleGitsigns()<CR>", { noremap = true, silent = true })
--- UNSURE
-vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#D2E59D" })
 
 ----------------                              ----------------
 
