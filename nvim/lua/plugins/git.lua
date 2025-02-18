ecbc9f7e064324d6ea046a806339420477f74404
Angel J. Haro <haro.j.angel@gmail.com>
2025-02-17 20:05:47 -0800

    git/ | git

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,13 +1,5 @@
+-- lua/plugins/git.lua
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  dependencies = {
-    "lewis6991/gitsigns.nvim",
-  },
-  config = function()
-    -- <C-[> breaks Nvim
-    require("plugins.git.copilot")
-    require("plugins.git.gitsings")
-  end,
+  require("plugins.git.copilot"),
+  require("plugins.git.gitsings")
 }
