b60b795a2b803e73189a27cebde5bca725520c40
Angel J. Haro <haro.j.angel@gmail.com>
2025-03-08 21:00:12 -0800

    removed required and instead passed by [depen]

--- nvim/lua/plugins/ricebox.lua
+++ nvim/lua/plugins/ricebox.lua
@@ -5,7 +5,7 @@ return {
   require("plugins.ricebox.none-ls"),           -- linting/formatting
   require("plugins.ricebox.toggle-diagnostics"), -- toggle lsp
   require("plugins.ricebox.autopair"),          -- pair ({[]})
-  require("plugins.ricebox.jdtls"),             -- java lsp
-  require("plugins.ricebox.csharp-lsp"),        --  c-sharp lsp
+  -- require("plugins.ricebox.jdtls"),             -- java lsp (we instead use it as a dependency)
+  -- require("plugins.ricebox.omnisharp"),        --  c-sharp lsp (we instead use it as a dependency)
   -- require("plugins.ricebox."),
 }
