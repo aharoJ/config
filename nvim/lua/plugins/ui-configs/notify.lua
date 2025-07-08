259d678c62b48376ceeb4a679bead6f9139b285c
Angel J. Haro <haro.j.angel@gmail.com>
2025-07-08 10:14:47 -0700

    debugging

--- nvim/lua/plugins/ui-configs/notify.lua
+++ nvim/lua/plugins/ui-configs/notify.lua
@@ -2,13 +2,13 @@
 require("notify").setup({
   background_colour = "#000000", -- transparent
   fps = 30,
-  icons = {
-    DEBUG = "",
-    ERROR = "",
-    INFO = "",
-    TRACE = "✎",
-    WARN = "",
-  },
+--   icons = {
+--     DEBUG = "",
+--     ERROR = "",
+--     INFO = "",
+--     TRACE = "✎",
+--     WARN = "",
+--   },
   level = 2,
   minimum_width = 30,
 	maximum_width = 30,
