04f4abe704f4a4f17f33e215b6c1fba69905e25c
Angel J. Haro <haro.j.angel@gmail.com>
2025-07-08 10:16:33 -0700

    .bak

--- nvim/lua/plugins/ricebox/debugging.lua
+++ nvim/lua/plugins/ricebox/debugging.lua
@@ -1,31 +1,18 @@
--- path: ~/.config/nvim/lua/plugins/ricebox/debugging.lua
-return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"theHamsta/nvim-dap-virtual-text",
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		-- -- "folke/neodev.nvim" -- neodev recommended HERE thats why we added it to begin with
-	},
-	config = function()
-		require("nvim-dap-virtual-text").setup()
-		require("dapui").setup()
+local M = {}
 
-		-------------------    DAP KEYBINDS    ------------------------
-		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-		vim.keymap.set("n", "<Leader>di", ":DapStepInto<CR>")
-		vim.keymap.set("n", "<Leader>dO", ":DapStepOut<CR>")
-		vim.keymap.set("n", "<leader>dd", require("dapui").toggle)
-		vim.keymap.set("n", "<leader>dD", ":DapVirtualTextToggle<CR>")
-		-- vim.keymap.set("n", "<leader>__", ":DapRestartFrame<CR>")
-		-- vim.keymap.set("n", "<leader>__", ":DapToggleRepl<CR>")
-		-- vim.keymap.set("n", "<leader>__", ":DapSetLogLevel<CR>")
-		-- vim.keymap.set("n", "<leader>__", ":DapLoadLaunchJSON<CR>")
-		-- vim.keymap.set("n", "<leader>__", ":DapShowLog<CR>")
-		-- vim.keymap.set("n", "<leader>__", ":DapVirtualTextToggle<CR>")
-		----------------                              ----------------
-	end,
-}
+function M.setup()
+    -- Debug mappings
+    vim.keymap.set("n", "<leader>da", require("jdtls.dap").test_class, { desc = "Debug test class" })
+    vim.keymap.set("n", "<leader>b", require("dap").toggle_breakpoint, { desc = "Toggle breakpoint" })
+
+    -- Test mappings
+    vim.keymap.set("n", "<leader>tm", require("jdtls").test_nearest_method, { desc = "Test method" })
+    vim.keymap.set("n", "<leader>tc", require("jdtls").test_class, { desc = "Test class" })
+
+    -- Spring Boot
+    vim.keymap.set("n", "<leader>sb", function()
+        require("jdtls").pick_test({ kind = "springboot" })
+    end, { desc = "Spring Boot" })
+end
+
+return M
