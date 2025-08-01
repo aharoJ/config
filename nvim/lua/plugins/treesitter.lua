d3033aeb047195b21862f0436c3917bf03851f8a
Angel J. Haro <haro.j.angel@gmail.com>
2025-08-01 13:07:48 -0700

    modern uptake

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,78 @@
+-- path: nvim/lua/plugins/treesitter.lua
+
+-- path: nvim/lua/plugins/treesitter.lua
 return {
   "nvim-treesitter/nvim-treesitter",
-  run = ":TSUpdate",
+  build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-context",
+    "windwp/nvim-ts-autotag",
+  },
 
   config = function()
+    ---------------------------------------------------------------------------
+    -- Treesitter core ---------------------------------------------------------
+    ---------------------------------------------------------------------------
     local configs = require("nvim-treesitter.configs")
+
     configs.setup({
       sync_install = false,
       auto_install = true,
-      ensure_installed = { "lua", "python", "json", "http", "typescript", "xml" },
-      -- ignore_install = { "javascript" },
+      ensure_installed = {
+        "lua",
+        "python",
+        "json",
+        "http",
+        "typescript",
+        "javascript",
+        "tsx",
+        "html",
+        "css",
+        "xml",
+        "vim",
+        "vimdoc",
+        "markdown_inline",
+        "query",
+        "java",
+      },
 
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "gnn", -- set to `false` to disable one of the mappings
+          init_selection = "gnn",
           node_incremental = "grn",
           scope_incremental = "grc",
           node_decremental = "grm",
         },
       },
 
-      autotag = {
-        enable = true,
-      },
-
-      indent = {
-        enable = true,
-      },
-
+      indent = { enable = true },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
+        disable = function(_, buf)
+          local max = 100 * 1024 -- 100 KB
+          local ok, stat = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          return ok and stat and stat.size > max
+        end,
       },
+    })
 
-      -- Treesitter Inspect
-      -- vim.keymap.set("n", "<Leader>mm", "<cmd>Inspect<CR>", {
-      --   desc = "Treesitter Inspect Node",
-      --   noremap = true,
-      --   silent = true,
-      -- }),
+    -- Handy inspection key
+    vim.keymap.set("n", "<Leader>gx", "<cmd>Inspect<CR>", {
+      desc = "Treesitter Inspect Node",
+      noremap = true,
+      silent = true,
+    })
+
+    require("nvim-ts-autotag").setup({
+      -- global options go under `opts`
+      opts = {
+        enable_close = true,      -- <div → <div></div>
+        enable_rename = true,     -- rename both tags
+        enable_close_on_slash = true, -- </ auto‑closes
+      },
     })
   end,
 }
