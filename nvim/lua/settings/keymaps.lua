222ea3fd49792eeb2bcd243bcbda2a34ea7d3170
Angel J. Haro <haro.j.angel@gmail.com>
2025-08-01 13:05:21 -0700

    cleaned up dead code

--- nvim/lua/settings/keymaps.lua
+++ nvim/lua/settings/keymaps.lua
@@ -58,4 +58,3 @@ vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-
