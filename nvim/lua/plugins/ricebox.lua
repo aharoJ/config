3fae5a853d57f89ce25b1b0cb9f60289a8a4b233
Angel J. Haro <haro.j.angel@gmail.com>
2025-08-01 13:07:08 -0700

    removed dead weight

--- nvim/lua/plugins/ricebox.lua
+++ nvim/lua/plugins/ricebox.lua
@@ -2,7 +2,4 @@
 return {
 	require("plugins.ricebox.lsp-config"), -- lsp configuration
 	require("plugins.ricebox.none-ls"), -- linting/formatting
-	require("plugins.ricebox.autopair"), -- pair ({[]})
-	--   require("plugins.ricebox.nvim-jdtls"),
-	-- require("plugins.ricebox.nvim-java"),
 }
