8e8bd59fab7f02634e5e6d7e6a165b06b608756b
Angel J. Haro <haro.j.angel@gmail.com>
2025-08-27 10:08:36 -0700

    fish: update according to lsp and diagnostics

--- fish/config.fish
+++ fish/config.fish
@@ -1,76 +1,53 @@
-######## Commands to run in interactive sessions can go here ########
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Interactive session setup ~~~~~~~~~~~~~~~~~~~~~ #
 if status is-interactive
-
-    # Editor and greeting
-    set -x EDITOR nvim
-    set fish_greeting
+    # Environment variables
+    set -gx EDITOR nvim # Default editor
+    set -gx fish_greeting # Disable Fish greeting
+    set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml # Starship prompt config
+    set -gx ANDROID_HOME ~/Library/Android/sdk # Android SDK for mobile dev
+    set -gx GPG_TTY (tty) # GPG for GitHub
 
     # Initialize Starship prompt
     starship init fish | source
-
-    # Ensure Homebrew Python 3.12 has priority
-    set -U fish_user_paths /usr/local/opt/python@3.12/libexec/bin $fish_user_paths
-
-    # Cargo (Rust) binaries
-    set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-
-    # pipx binaries
-    set -U fish_user_paths $HOME/.local/bin $fish_user_paths
-
-    # Emacs (MacPorts) - remove if you're no longer using MacPorts Emacs
-    set -U fish_user_paths "/Applications/MacPorts/Emacs.app/Contents/MacOS" $fish_user_paths
-
-    # JAVA configuration (default: OpenJDK 21)
-    set -gx JAVA_HOME (brew --prefix openjdk@21)
-    set -gx PATH $JAVA_HOME/bin $PATH
-
-    # Starship configuration file path
-    set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
-
-    # Android SDK Path (for mobile dev, C# MAUI)
-    set -gx ANDROID_HOME ~/Library/Android/sdk
-
-    # GPG configuration for GitHub
-    set -x GPG_TTY (tty)
-
 end
+# ~~~~~~~~~~~~~~~~~~~           ..................       ~~~~~~~~~~~~~~~~~~~ #
 
-# Set Neovim as default editor
-function vim
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~      Path      ~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+fish_add_path /usr/local/opt/python@3.12/libexec/bin # Homebrew Python 3.12
+fish_add_path $HOME/.cargo/bin # Cargo (Rust) binaries
+fish_add_path $HOME/.local/bin # pipx binaries
+# ~~~~~~~~~~~~~~~~~~~           ..................       ~~~~~~~~~~~~~~~~~~~ #
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Java setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# Java configuration (OpenJDK 21)
+set -gx JAVA_HOME (brew --prefix openjdk@21)
+set -gx PATH $JAVA_HOME/bin $PATH
+# ~~~~~~~~~~~~~~~~~~~           ..................       ~~~~~~~~~~~~~~~~~~~ #
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+function vim --description 'Use Neovim as vim'
     nvim $argv
 end
 
-# Set JAVA_HOME correctly inside TMUX sessions
+function v --description 'Use Neovim as v'
+    nvim $argv
+end
+
+function ys --description 'Start yabai service'
+    yabai --start-service
+end
+
+function yr --description 'Restart yabai service'
+    yabai --restart-service
+end
+
+function yS --description 'Stop yabai service'
+    yabai --stop-service
+end
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ TMUX-specific setup ~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 if test -n "$TMUX"
     set -gx JAVA_HOME (brew --prefix openjdk@21)
     set -gx PATH $JAVA_HOME/bin $PATH
 end
-
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ TMUX | PYTHON VENV STUFF ~~~~~~~~~~~~~~~~~~~~~ #
-# set -g project_dir "$HOME/desk/development/python/dcrm-project"
-#
-# function __auto_venv --on-variable PWD
-#     # Check if we're in project root OR any subdirectory
-#     if string match -q "$project_dir*" "$PWD"
-#         # Only activate if venv exists and not already active
-#         if test -e "$project_dir/denv/bin/activate.fish"
-#             if not set -q VIRTUAL_ENV
-#                 echo "Activating project virtual environment..."
-#                 source "$project_dir/denv/bin/activate.fish"
-#             end
-#         end
-#     else
-#         # Deactivate only if leaving project entirely
-#         if set -q VIRTUAL_ENV
-#             echo "Deactivating virtual environment..."
-#             deactivate
-#         end
-#     end
-# end
-#
-# # Initial check when shell starts
-# __auto_venv
-
-# DOES NOT WORK WITH TMUX SOOO I WILL NOT USE IT NO MORE 
 # ~~~~~~~~~~~~~~~~~~~           ..................       ~~~~~~~~~~~~~~~~~~~ #
