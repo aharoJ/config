f57898c7bc422390e30a6798828e8336c0dd0a5f
Angel J. Haro <haro.j.angel@gmail.com>
2025-10-05 03:17:22 -0700

    yazi (:

---
+++ nvim/lua/plugins/ui/yazi.lua
@@ -0,0 +1,14 @@
+-- path: nvim/lua/plugins/ui/yazi.lua
+---@diagnostic disable: undefined-global, undefined-doc-name
+---@type LazySpec
+
+return {
+    "mikavilpas/yazi.nvim",
+    keys = {
+        { "<leader>e",  "<cmd>Yazi<cr>" },
+        -- { "<leader>cw", "<cmd>Yazi cwd<cr>",    desc = "Yazi (at CWD)" },
+        -- { "<C-Up>",     "<cmd>Yazi toggle<cr>", desc = "Yazi resume" },
+    },
+    cmd = { "Yazi" },
+    opts = {},
+}
