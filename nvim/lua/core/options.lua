68a7840cf8ffa699796ad683028fc9d8d24971c1
Angel J. Haro <haro.j.angel@gmail.com>
2025-10-05 03:12:45 -0700

    opt

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,27 @@
+-- path: nvim/lua/core/options.lua
+---@diagnostic disable: undefined-global
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- vim.opt.scrolloff = 999
+vim.opt.scrolloff = 20
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+-- Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
