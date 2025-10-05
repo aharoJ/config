9761c2d86395868ba6027b7e007029d76ec14925
Angel J. Haro <haro.j.angel@gmail.com>
2025-10-05 03:12:40 -0700

    keymaps

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,25 @@
+-- path: nvim/lua/core/keymaps.lua
+---@diagnostic disable: undefined-global
+
+-- ── Indentation ───────────────────────────────────────────────────────────────────
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+-- ── File Operations ───────────────────────────────────────────────────────────────────
+vim.keymap.set("n", "<Leader>w", ":w<CR>", { desc = "write" })
+vim.keymap.set("n", "<Leader>q", ":q!<CR>", { desc = "quit" })
+vim.keymap.set("n", "<Leader>Q", ":qa!<CR>", { desc = "!QUIT" })
+-- vim.keymap.set("n", "<leader>X", "<cmd>silent! x<CR>", { desc = "Save and quit silently" })
+vim.keymap.set("n", "<leader>X", function() local ok = pcall(vim.cmd, "wall") if ok then vim.cmd("qall") else vim.notify("Write failed somewhere — not quitting.", vim.log.levels.ERROR) end end, { desc = "Write ALL and quit (safe)" })
+
+-- ── Buffers ───────────────────────────────────────────────────────────────────
+vim.keymap.set("n", "<S-h>", "<cmd>bprevious<CR>", { desc = "[buf] prev" })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<CR>", { desc = "[buf] next" })
+vim.keymap.set("n", "<leader>bc", "<cmd>bd<CR>", { desc = "[buf] close" })
+vim.keymap.set("n", "<leader>bC", "<cmd>%bd|e#<CR>", { desc = "[buf] close ." })
+
+
+-- ── HARDCORE EDITING ───────────────────────────────────────────────────────────────────
+-- vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+-- vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
