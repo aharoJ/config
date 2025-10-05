1c19a18cd1b669d14343b99444f9cfe97354ada6
Angel J. Haro <haro.j.angel@gmail.com>
2025-10-05 03:00:02 -0700

    del

--- tmux/scripts/truncate_path.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Truncate the path to show only the last two directories
-
-path=$1
-truncated_path=$(echo "$path" | awk -F'/' '{print $(NF-1)"/"$(NF)}')
-echo "../$truncated_path"
--- tmux/scripts/uptime.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uptime | rev | cut -d":" -f1 | rev | sed s/,//g
