b629826a965d5ca43e0e6f8dfb280ac263f3cb94
Angel J. Haro <haro.j.angel@gmail.com>
2025-11-05 12:47:27 -0800

    AMAZING productivity!

--- nvim/lua/core/paths.lua
+++ nvim/lua/core/paths.lua
@@ -1,31 +1,45 @@
--- Copy Full File-Path
-vim.keymap.set("n", "<leader>gp", function()
+-- path: nvim/lua/core/paths.lua
+
+-- Copy Full Absolute File-Path
+vim.keymap.set("n", "<leader>gP", function()
 	local path = vim.fn.expand("%:p")
 	vim.fn.setreg("+", path)
-	print("file:", path)
+	vim.notify("Copied: " .. path, vim.log.levels.INFO)
 end, {
-	desc = "[options] get pwd",
+	desc = "Copy full absolute path",
 })
 
--- Reveal in Finder/Explorer function
-local function reveal_in_file_manager()
-	local file = vim.fn.expand("%:p")
-	if file == "" then
-		vim.notify("No file to reveal (unsaved buffer?)", vim.log.levels.WARN)
+-- Copy Src Relative Path as Comment
+vim.keymap.set("n", "<leader>gp", function()
+	local full_path = vim.fn.expand("%:p")
+	local src_path = full_path:match("(src/.*)")
+
+	if not src_path then
+		vim.notify("Not inside src/ directory", vim.log.levels.WARN)
 		return
 	end
 
-	if vim.fn.has("mac") == 1 then
-		vim.fn.jobstart({ "open", "-R", file }, { detach = true })
-	else
-		-- Linux: open parent directory
-		local dir = vim.fn.fnamemodify(file, ":h")
-		vim.fn.jobstart({ "xdg-open", dir }, { detach = true })
-	end
-	vim.notify("Revealing: " .. file, vim.log.levels.INFO)
-end
+	local comment_line = "// path: " .. src_path
+	vim.fn.setreg("+", comment_line)
 
--- Keymap with improved description
-vim.keymap.set("n", "<leader>gP", reveal_in_file_manager, {
-	desc = "Reveal in Finder/Explorer",
+	local current_line = vim.fn.line(".")
+	vim.fn.append(current_line - 1, comment_line)
+
+	vim.notify("Inserted: " .. comment_line, vim.log.levels.INFO)
+end, {
+	desc = "Insert src relative path comment",
+})
+
+-- Reveal in Finder
+vim.keymap.set("n", "<leader>gr", function()
+	local file = vim.fn.expand("%:p")
+	if file == "" then
+		vim.notify("No file to reveal", vim.log.levels.WARN)
+		return
+	end
+
+	vim.fn.jobstart({ "open", "-R", file }, { detach = true })
+	vim.notify("Revealing: " .. file, vim.log.levels.INFO)
+end, {
+	desc = "Reveal in Finder",
 })
