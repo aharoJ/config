f91ecf891b200daddf6b941437a17d0d2f0c0b1f
Angel J. Haro <haro.j.angel@gmail.com>
2026-02-06 19:11:44 -0800

    path

--- fish/internal/yabai/yk.fish
+++ fish/internal/yabai/yk.fish
@@ -1,3 +1,4 @@
+# path: ~/.config/fish/internal/yabai/yk.fish
 function yk --description 'yabai: stop service'
     yabai --stop-service
 end
--- fish/internal/yabai/ys.fish
+++ fish/internal/yabai/ys.fish
@@ -1,3 +1,4 @@
+# path: ~/.config/fish/internal/yabai/ys.fish
 function ys --description 'yabai: start service'
     yabai --start-service
 end
