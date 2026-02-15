9ba50a4ec6509505f8a4392d67290b2421b11aed
Angel J. Haro <haro.j.angel@gmail.com>
2026-02-14 22:40:01 -0800

    enhanced multi-row view [get]

--- nvim-rebuild/lua/plugins/core/telescope.lua
+++ nvim-rebuild/lua/plugins/core/telescope.lua
@@ -1,7 +1,7 @@
 -- path: nvim-rebuild/lua/plugins/core/telescope.lua
 
--- STRATEGY
 local find_files_opts = {
+  -- STRATEGY
     layout_strategy = "center",
 
     -- CONFIG
@@ -103,6 +103,25 @@ local live_grep_opts = {
     sorting_strategy = "ascending",
 }
 
+local diagnostics_opts = {
+    layout_strategy = "vertical",
+    layout_config = {
+        width = 0.8,
+        height = 0.9,
+        preview_height = 0.4,
+        preview_cutoff = 1,  -- forces preview to ALWAYS show
+        prompt_position = "top",
+        mirror = true,
+    },
+    border = true,
+    borderchars = { "━", "┃", "━", "┃", "┏", "┓", "┛", "┗" },
+    winblend = 10,
+    prompt_prefix = "  ",
+    selection_caret = " ▸ ",
+    path_display = { "filename_first" },
+    sorting_strategy = "ascending",
+}
+
 -- preview_cutoff = 1,  -- forces preview to ALWAYS show
 -- Screen
 --  └─ height (total picker size)
@@ -117,11 +136,22 @@ return {
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     keys = {
+      -- GET
         { "<leader>gf", function() require("telescope.builtin").find_files(find_files_opts) end, desc = "[get] file", },
         { "<leader>gb", function() require("telescope.builtin").buffers(buffers_opts) end, desc = "[get] buffer", },
         { "<leader>g,", function() require("telescope.builtin").current_buffer_fuzzy_find(current_buffer_fuzzy_find_opts) end, desc = "[get] ," },
         { "<leader>gw", function() require("telescope.builtin").grep_string(grep_string_opts) end, desc = "[get] word" },
         { "<leader>g.", function() require("telescope.builtin").live_grep(live_grep_opts) end, desc = "[get] grep" },
+
+        -- LSP
+        { "<leader>df", function() require("telescope.builtin").diagnostics(vim.tbl_extend("force", diagnostics_opts, { bufnr = 0 })) end, desc = "[diagnostics] find (buffer)" },
+        { "<leader>dF", function() require("telescope.builtin").diagnostics(diagnostics_opts) end, desc = "[diagnostics] find (workspace)" },
+        { "<leader>gr", function() require("telescope.builtin").lsp_references(live_grep_opts) end, desc = "[get] references" },
+        { "<leader>gd", function() require("telescope.builtin").lsp_definitions(live_grep_opts) end, desc = "[get] definitions" },
+        { "<leader>gi", function() require("telescope.builtin").lsp_implementations(live_grep_opts) end, desc = "[get] implementations" },
+        { "<leader>gt", function() require("telescope.builtin").lsp_type_definitions(live_grep_opts) end, desc = "[get] type definitions" },
+        { "<leader>gs", function() require("telescope.builtin").lsp_document_symbols(live_grep_opts) end, desc = "[get] document symbols" },
+        { "<leader>gS", function() require("telescope.builtin").lsp_dynamic_workspace_symbols(live_grep_opts) end, desc = "[get] workspace symbols" },
     },
     config = function()
         require("telescope").setup({
